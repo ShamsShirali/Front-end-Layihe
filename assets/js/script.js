8977dc3f17fa3fbe51c82bcb386a346c9356642e
unknown <shamsshsh@code.edu.az>
2023-04-21 23:35:14 +0400

    slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,12 @@
 
 $(function () {
 
-  $('.first_section_slider').slick({
-      centerMode: true,
-      centerPadding: '0px',
-      slidesToShow: 1,
-      responsive: [
-          {
-              breakpoint: 768,
-              settings: {
-                  arrows: false,
-                  centerMode: true,
-                  centerPadding: '40px',
-                  slidesToShow: 3
-              }
-          },
-          {
-              breakpoint: 480,
-              settings: {
-                  arrows: false,
-                  centerMode: true,
-                  centerPadding: '40px',
-                  slidesToShow: 1
-              }
-          }
-      ]
-  });
+  $('.slick_slide').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+});
+
  
  
 })
