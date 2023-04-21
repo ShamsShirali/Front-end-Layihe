50e07bb452c1a0a24aee4ef1cae6e51cedcb95cc
unknown <shamsshsh@code.edu.az>
2023-04-21 23:20:22 +0400

    slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,39 @@
 "use strict";
 
+$(function () {
+
+  $('.first_section_slider').slick({
+      centerMode: true,
+      centerPadding: '0px',
+      slidesToShow: 1,
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+              }
+          }
+      ]
+  });
+ 
+ 
+})
+
+
+
+
 
 
 
