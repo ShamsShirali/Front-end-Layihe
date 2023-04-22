b5aa18a46e36426bec4f7f9fc11508ff5443c1ee
unknown <shamsshsh@code.edu.az>
2023-04-22 13:16:48 +0400

    carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,40 @@ $(function () {
     autoplaySpeed: 5000
 });
 
- 
- 
+$('.slide2').slick({
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 1,
+  responsive: [
+      {
+          breakpoint: 768,
+          settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+          }
+      }
+  ]
+});
+
+$('icons').click(function(){
+$('.icons').css({
+  'background-color' : 'black',
+  'color' : 'white'
 })
+});
+})
+
 
 
 
