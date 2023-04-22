4ef4cacc2e6460845066bb28d078708547ee4e03
unknown <shamsshsh@code.edu.az>
2023-04-23 02:56:26 +0400

    cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
-$('.nsecond .secondl ul .category').each(function () {
+$('.nsecond .secondl .category').each(function () {
   $(this).click(function () {
-    if ($(this).attr("data-id") == 5) {
+    if ($(this).attr("data-id") == 6) {
       $(this).prev().css("background-color", "white");
       $(this).prev().css("color", "grey");
       $(this).prev().prev().css("background-color", "white");
@@ -11,31 +11,58 @@ $('.nsecond .secondl ul .category').each(function () {
       $(this).prev().prev().prev().css("color", "grey");
       $(this).prev().prev().prev().prev().css("background-color", "white");
       $(this).prev().prev().prev().prev().css("color", "grey");
+      $(this).prev().prev().prev().prev().prev().css("background-color", "white");
+      $(this).prev().prev().prev().prev().prev().css("color", "grey");
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-      $('.nsecond .slick-dots').children().eq(4).children().eq(0).click();
-      if ($('.nsecond .select .line .underline').css('transform', 'translateX(0px)')) {
-        $('.nsecond .select .line .underline').css('transform', 'translateX(490px)')
+      $('.nsecond .slick-dots').children().eq(5).children().eq(0).click();
+      if ($('.nsecond .line .underline').css('transform', 'translateX(0px)')) {
+        $('.nsecond .line .underline').css('transform', 'translateX(520px)')
       }
       else {
-        $('.nsecond .select .line .underline').css('transform', 'translateX(300px)')
+        $('.nsecond .line .underline').css('transform', 'translateX(300px)')
       }
     }
-    if ($(this).attr("data-id") == 4) {
-      $(this).prev().css("background-color", "white");
-      $(this).prev().css("color", "grey");
-      $(this).prev().prev().css("background-color", "white");
-      $(this).prev().prev().css("color", "grey");
-      $(this).prev().prev().prev().css("background-color", "white");
-      $(this).prev().prev().prev().css("color", "grey");
-      $(this).css("background-color", "white");
-      $(this).css("color", "black");
-      $('three .slick-dots').children().eq(3).children().eq(0).click();
-      if ($('.three .select .line .underline').css('transform', 'translateX(0px)')) {
-        $('.three .select .line .underline').css('transform', 'translateX(490px)')
+    if ($(".nsecond .secondl .category .shop").attr("data-id") == 5) {
+      $(".nsecond .secondl .category .shop").prev().css("background-color", "white");
+      $(".nsecond .secondl .category .shop").prev().css("color", "grey");
+      $(".nsecond .secondl .category .shop").prev().prev().css("background-color", "white");
+      $(".nsecond .secondl .category .shop").prev().prev().css("color", "grey");
+      $(".nsecond .secondl .category .shop").prev().prev().prev().css("background-color", "white");
+      $(".nsecond .secondl .category .shop").prev().prev().prev().css("color", "grey");
+      $(".nsecond .secondl .category .shop").prev().prev().prev().prev().css("background-color", "white");
+      $(".nsecond .secondl .category .shop").prev().prev().prev().prev().css("color", "grey");
+      $(".nsecond .secondl .category .shop").next().css("background-color", "white");
+      $(".nsecond .secondl .category .shop").next().css("color", "grey");
+      $(".nsecond .secondl .category .shop").css("background-color", "white");
+      $(".nsecond .secondl .category .shop").css("color", "black");
+      $('.nsecond .slick-dots').children().eq(4).children().eq(0).click();
+      if ($('.nsecond .line .underline').css('transform', 'translateX(0px)')) {
+        $('.nsecond .line .underline').css('transform', 'translateX(490px)')
       }
       else {
-        $('.three .select .line .underline').css('transform', 'translateX(300px)')
+        $('.nsecond .line .underline').css('transform', 'translateX(300px)')
+      }
+    }
+    if ($(".nsecond .secondl .category .search").attr("data-id") == 4) {
+      $(".nsecond .secondl .category .search").prev().css("background-color", "white");
+      $(".nsecond .secondl .category .search").prev().css("color", "grey");
+      $(".nsecond .secondl .category .search").prev().prev().css("background-color", "white");
+      $(".nsecond .secondl .category .search").prev().prev().css("color", "grey");
+      $(".nsecond .secondl .category .search").prev().prev().prev().css("background-color", "white");
+      $(".nsecond .secondl .category .search").prev().prev().prev().css("color", "grey");
+      $(".nsecond .secondl .category .search").next().css("background-color", "white");
+      $(".nsecond .secondl .category .search").next().css("color", "grey");
+      $(".nsecond .secondl .category .search").next().next().css("background-color", "white");
+      $(".nsecond .secondl .category .search").next().next().css("color", "grey");
+      $(".nsecond .secondl .category .search").css("background-color", "white");
+      $(".nsecond .secondl .category .search").css("color", "black");
+      $('.nsecond .slick-dots').children().eq(3).children().eq(0).click();
+      if ($('.nsecond .line .underline').css('transform', 'translateX(0px)')) {
+        $('.nsecond .line .underline').css('transform', 'translateX(350px)')
+      }
+      else {
+        $('.nsecond .line .underline').css('transform', 'translateX(300px)')
       }
     }
     if ($(this).attr("data-id") == 3) {
@@ -45,14 +72,18 @@ $('.nsecond .secondl ul .category').each(function () {
       $(this).prev().prev().css("color", "grey");
       $(this).next().css("background-color", "white");
       $(this).next().css("color", "grey");
+      $(this).next().next().css("background-color", "white");
+      $(this).next().next().css("color", "grey");
+      $(this).next().next().next().next().css("background-color", "white");
+      $(this).next().next().next().next().css("color", "grey");
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-      $('three .slick-dots').children().eq(2).children().eq(0).click();
-      if ($('.three .select .line .underline').css('transform', 'translateX(0px)')) {
-        $('.three .select .line .underline').css('transform', 'translateX(320px)')
+      $('.nsecond .slick-dots').children().eq(2).children().eq(0).click();
+      if ($('.nsecond .line .underline').css('transform', 'translateX(0px)')) {
+        $('.nsecond .line .underline').css('transform', 'translateX(250px)')
       }
       else {
-        $('.three .select .line .underline').css('transform', 'translateX(200px)')
+        $('.nsecond .line .underline').css('transform', 'translateX(200px)')
       }
     }
     if ($(this).attr("data-id") == 2) {
@@ -62,14 +93,18 @@ $('.nsecond .secondl ul .category').each(function () {
       $(this).next().css("color", "grey");
       $(this).next().next().css("background-color", "white");
       $(this).next().next().css("color", "grey");
+      $(this).next().next().next().next().css("background-color", "white");
+      $(this).next().next().next().next().css("color", "grey");
+      $(this).next().next().next().next().next().css("background-color", "white");
+      $(this).next().next().next().next().next().css("color", "grey");
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-      $('three .slick-dots').children().eq(1).children().eq(0).click();
-      if ($('.three .select .line .underline').css('transform', 'translateX(0px)')) {
-        $('.three .select .line .underline').css('transform', 'translateX(150px)')
+      $('.nsecond .slick-dots').children().eq(1).children().eq(0).click();
+      if ($('.nsecond .line .underline').css('transform', 'translateX(0px)')) {
+        $('.nsecond .line .underline').css('transform', 'translateX(115px)')
       }
       else {
-        $('.three .select .line .underline').css('transform', 'translateX(-180px)')
+        $('.nsecond .line .underline').css('transform', 'translateX(-180px)')
       }
     }
     if ($(this).attr("data-id") == 1) {
@@ -79,10 +114,16 @@ $('.nsecond .secondl ul .category').each(function () {
       $(this).next().next().css("color", "grey");
       $(this).next().next().next().css("background-color", "white");
       $(this).next().next().next().css("color", "grey");
+      $(this).next().next().next().next().css("background-color", "white");
+      $(this).next().next().next().next().css("color", "grey");
+      $(this).next().next().next().next().next().css("background-color", "white");
+      $(this).next().next().next().next().next().css("color", "grey");
+      $(this).next().next().next().next().next().next().css("background-color", "white");
+      $(this).next().next().next().next().next().next().css("color", "grey");
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-      $('three .slick-dots').children().eq(0).children().eq(0).click();
-      $('.three .select .line .underline').css('transform', 'translateX(0px)');
+      $('.nsecond .slick-dots').children().eq(0).children().eq(0).click();
+      $('.nsecond .line .underline').css('transform', 'translateX(0px)');
     }
   })
 })
@@ -162,7 +203,7 @@ $('.three .select ul li').each(function () {
       $(this).next().css("color", "grey");
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-      $('three .slick-dots').children().eq(2).children().eq(0).click();
+      $('.three .slick-dots').children().eq(2).children().eq(0).click();
       if ($('.three .select .line .underline').css('transform', 'translateX(0px)')) {
         $('.three .select .line .underline').css('transform', 'translateX(320px)')
       }
@@ -179,7 +220,7 @@ $('.three .select ul li').each(function () {
       $(this).next().next().css("color", "grey");
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-      $('three .slick-dots').children().eq(1).children().eq(0).click();
+      $('.three .slick-dots').children().eq(1).children().eq(0).click();
       if ($('.three .select .line .underline').css('transform', 'translateX(0px)')) {
         $('.three .select .line .underline').css('transform', 'translateX(150px)')
       }
@@ -196,7 +237,7 @@ $('.three .select ul li').each(function () {
       $(this).next().next().next().css("color", "grey");
       $(this).css("background-color", "white");
       $(this).css("color", "black");
-      $('three .slick-dots').children().eq(0).children().eq(0).click();
+      $('.three .slick-dots').children().eq(0).children().eq(0).click();
       $('.three .select .line .underline').css('transform', 'translateX(0px)');
     }
   })
